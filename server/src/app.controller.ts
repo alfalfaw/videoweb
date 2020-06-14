33c7f62d0ecedcb9d1400840546fd0464418b224
alfalfaw <limuyle@163.com>
2020-06-14 23:36:43 +0800

    fix some bugs

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,50 +1,51 @@
-import { Controller, Post, UseInterceptors, UploadedFile, UseGuards, Request, Get, UploadedFiles } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { FileInterceptor, FileFieldsInterceptor } from "@nestjs/platform-express"
-import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from './auth/auth.service';
+import { Controller, Post, UseInterceptors, UploadedFile, UseGuards, Request, Get, UploadedFiles } from '@nestjs/common'
+import { ApiTags, ApiOperation } from '@nestjs/swagger'
+import { FileInterceptor, FileFieldsInterceptor } from '@nestjs/platform-express'
+import { AuthGuard } from '@nestjs/passport'
+import { AuthService } from './auth/auth.service'
 //
-import multer = require('multer');
+import multer = require('multer')
 @Controller()
 @ApiTags('App')
 export class AppController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   //无需jwt就能访问
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   @ApiOperation({ summary: '登陆' })
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    return this.authService.login(req.user)
   }
   //需要用jwt才能访问的api,用 @UseGuards(AuthGuard('jwt'))标注
   @UseGuards(AuthGuard('jwt'))
   @Get('profile')
   @ApiOperation({ summary: '获取用户资料' })
   getProfile(@Request() req) {
-    return req.user;
+    return req.user
   }
-
 
   //使用拦截器file是数据名
   // @UseGuards(AuthGuard('jwt'))
   @Post('upload')
   @ApiOperation({ summary: '上传文件' })
-  @UseInterceptors(FileInterceptor('file', {
-    storage: multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, 'uploads');
-      },
-      filename: (req, file, cb) => {
-        cb(null, (new Date()).getTime() + file.originalname.replace(/\s+/g, ""));
-      },
-    }),
-  }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+          cb(null, 'uploads')
+        },
+        filename: (req, file, cb) => {
+          cb(null, new Date().getTime() + file.originalname.replace(/\s+/g, ''))
+        }
+      })
+    })
+  )
   async upload(@UploadedFile('file') file) {
     //返回对象
     // return file
-    return { url: `http://localhost:5000/uploads/${file.filename}` }
-
+    // return { url: `http://localhost:5000/uploads/${file.filename}` }
+    return { url: `https://video.alfalfa.website/uploads/${file.filename}` }
   }
 
   // @Post('upload')
@@ -57,23 +58,29 @@ export class AppController {
 
   //上传多个文件
   @Post('uploadmany')
-  @UseInterceptors(FileFieldsInterceptor([
-    { name: 'image', maxCount: 1 },
-    { name: 'video', maxCount: 1 },
-  ], {
-    storage: multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, 'uploads');
-      },
-      filename: (req, file, cb) => {
-        cb(null, (new Date()).getTime() + file.originalname.replace(/\s+/g, ""));
-      },
-    }),
-  }))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'image', maxCount: 1 },
+        { name: 'video', maxCount: 1 }
+      ],
+      {
+        storage: multer.diskStorage({
+          destination: (req, file, cb) => {
+            cb(null, 'uploads')
+          },
+          filename: (req, file, cb) => {
+            cb(null, new Date().getTime() + file.originalname.replace(/\s+/g, ''))
+          }
+        })
+      }
+    )
+  )
   uploadmany(@UploadedFiles() files) {
     let cover = null
     let url = null
-    const base_url = 'http://localhost:5000/uploads/'
+    // const base_url = 'http://localhost:5000/uploads/'
+    const base_url = 'https://video.alfalfa.website/uploads/'
     let image = null
     let video = null
     if (files.image) {
@@ -109,8 +116,4 @@ export class AppController {
 
   //   return { cover: cover, url: url, success: true }
   // }
-
-
-
-
 }
